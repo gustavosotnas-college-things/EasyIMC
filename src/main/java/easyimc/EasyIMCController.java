75c32c08edf6b0417f10a98dab1ceb3dfc8ef49b
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-06-15 16:50:53 -0300

    Copiado código do Model e Controller do CalculadoraServlet (walisonmoreira/ufg-inf-dw-2016-1)

--- src/main/java/easyimc/EasyIMCController.java
+++ src/main/java/easyimc/EasyIMCController.java
@@ -1,5 +1,43 @@
 package easyimc;
 
-public class EasyIMCController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/EasyIMC")
+public class EasyIMCController extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int oper1 = toInt(req, "operador1", "0");
+		String op = valor(req, "operacao", "");
+		int oper2 = toInt(req, "operador2", "0");
+
+		int resultadoCalculo = CalculadoraModel.calcular(oper1, op, oper2);
+		
+		//Passando parâmetro para o JSP.
+		req.setAttribute(
+				"resultado",
+				resultadoCalculo);
+
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
+	}
+	
+	private int toInt(HttpServletRequest req, String param, String padrao)
+	{
+		return Integer.parseInt(valor(req, param, padrao));
+	}
+	
+	private String valor(HttpServletRequest req, String param, String padrao) {
+
+		String result = req.getParameter(param);
+		if (result == null) {
+			result = padrao;
+		}
+		return result;
+	}
 }
--- src/main/java/easyimc/EasyIMCModel.java
+++ src/main/java/easyimc/EasyIMCModel.java
@@ -2,4 +2,15 @@ package easyimc;
 
 public class EasyIMCModel {
 
+	public static int calcular(int oper1, String op, int oper2) {
+		int resultado = 0;
+		if (op.equals("+")) {
+			resultado = oper1 + oper2;
+		} else if (op.equals("-")) {
+			resultado = oper1 - oper2;
+		} else if (op.equals("*")) {
+			resultado = oper1 * oper2;
+		}
+		return resultado;
+	}
 }
